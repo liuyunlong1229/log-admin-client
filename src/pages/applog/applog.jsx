dd8b20970923c74041d5a81cb2a4d1121d49148d
yunlongliu <Lyl&%#2020>
2021-04-16 19:24:46 +0800

    提交

---
+++ src/pages/applog/applog.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Applog extends Component {
+    render() {
+        return (
+            <div>
+                Applog
+            </div>
+        )
+    }
+}
---
+++ src/pages/command/command.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Command extends Component {
+    render() {
+        return (
+            <div>
+                hcommand
+            </div>
+        )
+    }
+}
